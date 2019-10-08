32fc22298d79b01a59e422b78025982bc4194f60
Elias Costa <elias.costa@quintoandar.com.br>
2019-10-08 18:41:27 -0300

    fix shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-export DATA_SOURCE_NAME="${DB_USER}:${DB_PASSWORD}@(${DB_DNS}:${DB_PORT:3306})/" && mysqld_exporter --collect.all
+export DATA_SOURCE_NAME="${DB_USER}:${DB_PASSWORD}@(${DB_DNS}:${DB_PORT:-3306})/" && mysqld_exporter --collect.all
